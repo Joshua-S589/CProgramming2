a66c6d9ca55f8f30b0304138be9daa35ba750ed8
Joshua-S589 <jskywalker8055@floridapoly.edu>
2026-02-13 18:21:53 +0000

    Multiple battles without restarting code implemented, done for now

--- programs/beyblade-x/beyblade-x-battle.c
+++ programs/beyblade-x/beyblade-x-battle.c
@@ -1,8 +1,10 @@
 #include "/workspaces/CProgramming2/programs/beyblade-x/deck-randomizer-beyblade-x.h"
 #include "/workspaces/CProgramming2/programs/beyblade-x/match-beyblade-x.h"
 void deckOrderRandomizer(void);
+void runBattle(void);
 char battleChoice;
 char battleResultsType;
+char playerChoice;
 int battleResultsPlayer;
 int deckChoice;
 int matchBattleNum=0;
@@ -57,98 +59,35 @@ int main(void){
     if(battleChoice=='B'){
         userSelect1();
         userSelect2();
-        makeTheCombos(2);
-        printf("How many points do you want to go to? ");
-        scanf(" %d", &pointGoal);
-        while(pointGoal>100){
-            puts("Sorry, going to that many points will take too long, please reenter");
-            printf("How many points do you want to go to? ");
-            scanf(" %d", &pointGoal);
-        }
-        while(pointGoal<1){
-            puts("Sorry, that is an invalid goal, please reenter");
-            printf("How many points do you want to go to? ");
-            scanf(" %d", &pointGoal);
-            while(pointGoal>100){
-                puts("Sorry, going to that many points will take too long, please reenter");
-                printf("How many points do you want to go to? ");
-                scanf(" %d", &pointGoal);
+        runBattle();
+        for(int a=0;a<1;){
+            printf("Would you like to continue to run battles? Y/N ");
+            scanf(" %c", &playerChoice);
+            while(!(playerChoice=='Y'||playerChoice=='N')){
+                puts("\nSorry, that is an invalid command. Please reenter");
+                printf("Would you like to continue to run battles? Y/N ");
+                scanf(" %c", &playerChoice);
             }
-        }
-        greaterPoint=0;
-        while(greaterPoint<pointGoal){
-            if(greaterPoint==0){
-                puts("Randomizing the deck orders for the begining of the match...");
-            }
-            else{
-                puts("End of decks has been reached, and the match is not yet finished. Re-randomizing the deck orders.");
-            }
-            deckOrderRandomizer();
-            do{
-                printf("Beyblades for battle %d: \n\n%s\nvs\n%s\n\n", matchBattleNumTotal, deckOrder1[matchBattleNum], deckOrder2[matchBattleNum]);
-                printf("Did player 1 or 2 win the battle? Player ");
-                scanf(" %d", &battleResultsPlayer);
-                while(!(battleResultsPlayer==1||battleResultsPlayer==2)){
-                    puts("Sorry, that is an invalid player number. Please reenter");
-                    printf("Did player 1 or 2 win the battle? Player ");
-                    scanf(" %d", &battleResultsPlayer);
+            if(playerChoice=='Y'){
+                printf("\nWould you like to keep with the same players (S), or generate decks under different players (D)? ");
+                scanf(" %c", &playerChoice);
+                while(!(playerChoice=='S'||playerChoice=='D')){
+                    puts("\nSorry, that is an invalid command. Please reenter");
+                    printf("Would you like to keep with the same players (S), or generate decks under different players (D)? ");
+                    scanf(" %c", &playerChoice);
                 }
-                printf("What is the outcome of the battle? Spin Finish (S), Burst Finish (B), Over Finish (O), or Extreme Finish (E): ");
-                scanf(" %c", &battleResultsType);
-                while((!battleResultsType=='S')&&(!battleResultsType=='B')&&(!battleResultsType=='O')&&(!battleResultsType=='E')){
-                    puts("Sorry, that is an invalid result, please reenter");
-                    printf("What is the outcome of the battle? Spin Finish (S), Burst Finish (B), Over Finish (O), or Extreme Finish (E): ");
-                    scanf(" %c", &battleResultsType);
-                }
-                if(battleResultsType=='S'){
-                    if(battleResultsPlayer==1){
-                        score1+=1;
-                    }
-                    if(battleResultsPlayer==2){
-                        score2+=1;
-                    }
-                }
-                if(battleResultsType=='B'){
-                    if(battleResultsPlayer==1){
-                        score1+=2;
-                    }
-                    if(battleResultsPlayer==2){
-                        score2+=2;
-                    }
-                }
-                if(battleResultsType=='O'){
-                    if(battleResultsPlayer==1){
-                        score1+=2;
-                    }
-                    if(battleResultsPlayer==2){
-                        score2+=2;
-                    }
-                }
-                if(battleResultsType=='E'){
-                    if(battleResultsPlayer==1){
-                        score1+=3;
-                    }
-                    if(battleResultsPlayer==2){
-                        score2+=3;
-                    }
-                }
-                printf("\nCurrent Scores:\nPlayer 1: %d\nPlayer 2: %d\n", score1, score2);
-                matchBattleNum++;
-                matchBattleNumTotal++;
-                if(score1>score2){
-                    greaterPoint=score1;
+                if(playerChoice=='S'){
+                    runBattle();
                 }
                 else{
-                    greaterPoint=score2;
+                    userSelect1();
+                    userSelect2();
+                    runBattle();
                 }
-            }while(greaterPoint<pointGoal&&matchBattleNum<finalDeckSize);
-            matchBattleNum=0;
-        }
-        if(score1>score2){
-            printf("Player 1 beat Player 2 with a final score of %d-%d\n", score1, score2);
-        }
-        else{
-            printf("Player 2 beat Player 1 with a final score of %d-%d\n", score2, score1);
+            }
+            else{
+                a++;
+            }
         }
     }
     return 0;
@@ -176,4 +115,100 @@ void deckOrderRandomizer(void){
         avbCombo1[j]=1;
         avbCombo2[j]=1;
     }
+}
+void runBattle(void){
+    makeTheCombos(2);
+    printf("How many points do you want to go to? ");
+    scanf(" %d", &pointGoal);
+    while(pointGoal>100){
+        puts("Sorry, going to that many points will take too long, please reenter");
+        printf("How many points do you want to go to? ");
+        scanf(" %d", &pointGoal);
+    }
+    while(pointGoal<1){
+        puts("Sorry, that is an invalid goal, please reenter");
+        printf("How many points do you want to go to? ");
+        scanf(" %d", &pointGoal);
+        while(pointGoal>100){
+            puts("Sorry, going to that many points will take too long, please reenter");
+            printf("How many points do you want to go to? ");
+            scanf(" %d", &pointGoal);
+        }
+    }
+    greaterPoint=0;
+    while(greaterPoint<pointGoal){
+        if(greaterPoint==0){
+            puts("Randomizing the deck orders for the begining of the match...");
+        }
+        else{
+            puts("End of decks has been reached, and the match is not yet finished. Re-randomizing the deck orders.");
+        }
+        deckOrderRandomizer();
+        do{
+            printf("Beyblades for battle %d: \n\n%s\nvs\n%s\n\n", matchBattleNumTotal, deckOrder1[matchBattleNum], deckOrder2[matchBattleNum]);
+            printf("Did player 1 or 2 win the battle? Player ");
+            scanf(" %d", &battleResultsPlayer);
+            while(!(battleResultsPlayer==1||battleResultsPlayer==2)){
+                puts("Sorry, that is an invalid player number. Please reenter");
+                printf("Did player 1 or 2 win the battle? Player ");
+                scanf(" %d", &battleResultsPlayer);
+            }
+            printf("What is the outcome of the battle? Spin Finish (S), Burst Finish (B), Over Finish (O), or Extreme Finish (E): ");
+            scanf(" %c", &battleResultsType);
+            while(!((battleResultsType=='S')||(battleResultsType=='B')||(battleResultsType=='O')||(battleResultsType=='E'))){
+                puts("Sorry, that is an invalid result, please reenter");
+                printf("What is the outcome of the battle? Spin Finish (S), Burst Finish (B), Over Finish (O), or Extreme Finish (E): ");
+                scanf(" %c", &battleResultsType);
+            }
+            if(battleResultsType=='S'){
+                if(battleResultsPlayer==1){
+                    score1+=1;
+                }
+                if(battleResultsPlayer==2){
+                    score2+=1;
+                }
+            }
+            if(battleResultsType=='B'){
+                if(battleResultsPlayer==1){
+                    score1+=2;
+                }
+                if(battleResultsPlayer==2){
+                    score2+=2;
+                }
+            }
+            if(battleResultsType=='O'){
+                if(battleResultsPlayer==1){
+                    score1+=2;
+                }
+                if(battleResultsPlayer==2){
+                    score2+=2;
+                }
+            }
+            if(battleResultsType=='E'){
+                if(battleResultsPlayer==1){
+                    score1+=3;
+                }
+                if(battleResultsPlayer==2){
+                    score2+=3;
+                }
+            }
+            printf("\nCurrent Scores:\nPlayer 1: %d\nPlayer 2: %d\n", score1, score2);
+            matchBattleNum++;
+            matchBattleNumTotal++;
+            if(score1>score2){
+                greaterPoint=score1;
+            }
+            else{
+                greaterPoint=score2;
+            }
+        }while(greaterPoint<pointGoal&&matchBattleNum<finalDeckSize);
+        matchBattleNum=0;
+    }
+    if(score1>score2){
+        printf("Player 1 beat Player 2 with a final score of %d-%d\n", score1, score2);
+    }
+    else{
+        printf("Player 2 beat Player 1 with a final score of %d-%d\n", score2, score1);
+    }
+    printf("\n\n");
 }
